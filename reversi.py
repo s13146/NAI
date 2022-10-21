745f70c52daede9e71a941328bfe80b1ecef93eb
KatarzynaW <56793702+s13146@users.noreply.github.com>
2022-10-21 19:07:49 +0200

    Add files via upload

--- reversi.py
+++ reversi.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Ilya Ryzhkov / Katarzyna Węsierska
+# Created Date: oct 2022
+# version ='0.1'
+# https://pl.wikipedia.org/wiki/Reversi
+# Required: easyAI
+# ---------------------------------------------------------------------------
+
 import numpy as np
 from easyAI import TwoPlayerGame
 
@@ -7,10 +17,19 @@ to_array = lambda s: np.array(["ABCDEFGH".index(s[0]), int(s[1]) - 1])
 
 class Reversi(TwoPlayerGame):
     """
-    Każdy z dwóch graczy ma do dyspozycji pionki: jeden koloru białego, drugi czarnego. Początkowo na planszy znajdują się po dwa pionki każdego z graczy. Gracze układają na przemian pionki własnego koloru na wolnych polach planszy do momentu, aż plansza zostanie całkowicie zapełniona lub żaden z graczy nie będzie mógł wykonać dozwolonego ruchu.
-    Dozwolony ruch to taki, w którym pionek jest ułożony na polu, które znajduje się w linii (poziomej, pionowej lub ukośnej) z innym pionkiem gracza wykonującego ruch, i na dokładnie wszystkich polach pomiędzy wybranym polem a tym pionkiem znajdują się pionki przeciwnika. Te pionki zostają po wykonaniu ruchu przejęte i zmieniają kolor na przeciwny.
-    Wygrywa ten z graczy, którego większa liczba pionków znajduje się na planszy po zakończeniu gry; jeśli liczba pionków graczy jest jednakowa, następuje remis.
-    :param players: players from easyAI framework
+    Każdy z dwóch graczy ma do dyspozycji pionki: jeden koloru białego,
+    drugi czarnego. Początkowo na planszy znajdują się po dwa pionki każdego
+    z graczy. Gracze układają na przemian pionki własnego koloru na wolnych
+    polach planszy do momentu, aż plansza zostanie całkowicie zapełniona lub
+    żaden z graczy nie będzie mógł wykonać dozwolonego ruchu. Dozwolony ruch
+    to taki, w którym pionek jest ułożony na polu, które znajduje się w
+    linii (poziomej, pionowej lub ukośnej) z innym pionkiem gracza
+    wykonującego ruch, i na dokładnie wszystkich polach pomiędzy wybranym
+    polem a tym pionkiem znajdują się pionki przeciwnika. Te pionki zostają
+    po wykonaniu ruchu przejęte i zmieniają kolor na przeciwny. Wygrywa ten
+    z graczy, którego większa liczba pionków znajduje się na planszy po
+    zakończeniu gry; jeśli liczba pionków graczy jest jednakowa, następuje
+    remis. :param players: players from easyAI framework.
     :param board: array passed to game
 
     """
@@ -23,14 +42,17 @@ class Reversi(TwoPlayerGame):
         self.current_player = 1
 
     def possible_moves(self):
-        """ Dozwolnone są tylko takie ruchy ktore prowadza do odwrocenia kolorow. 
-        :return: all moves allowed in the game """
+        """ Dozwolone są tylko takie ruchy które prowadza do odwrócenia
+        kolorów.
+        :return: all moves allowed in the game
+        """
         return [
             to_string((i, j))
             for i in range(8)
             for j in range(8)
             if (self.board[i, j] == 0)
-            and (pieces_flipped(self.board, (i, j), self.current_player) != [])
+               and (pieces_flipped(self.board, (i, j),
+                                   self.current_player) != [])
         ]
 
     def make_move(self, pos):
@@ -52,7 +74,8 @@ class Reversi(TwoPlayerGame):
                     "ABCDEFGH"[k]
                     + " "
                     + " ".join(
-                        [[".", "1", "2", "X"][self.board[k][i]] for i in range(8)]
+                        [[".", "1", "2", "X"][self.board[k][i]] for i in
+                         range(8)]
                     )
                     for k in range(8)
                 ]
@@ -67,11 +90,12 @@ class Reversi(TwoPlayerGame):
         return self.possible_moves() == []
 
     def scoring(self):
-        """ Poczatkowo zwraca sie uwage na rozmieszczenie pionkow, nastepnie tylko na to jaka liczbe pionkow na planszy ma dany gracz.
-        
+        """ Początkowo zwraca się uwage na rozmieszczenie pionkow,
+         nastepnie tylko na to jaka liczbe pionkow na planszy ma dany gracz.
         """
 
-        if np.sum(self.board == 0) > 32:  # mniej niz polowa tablicy jest pelna
+        if np.sum(self.board == 0) > 32:
+            # mniej niz polowa tablicy jest pelna
             player = (self.board == self.current_player).astype(int)
             opponent = (self.board == self.opponent_index).astype(int)
             return ((player - opponent) * BOARD_SCORE).sum()
@@ -96,13 +120,15 @@ BOARD_SCORE = np.array(
 )
 
 DIRECTIONS = [
-    np.array([i, j]) for i in [-1, 0, 1] for j in [-1, 0, 1] if (i != 0 or j != 0)
+    np.array([i, j]) for i in [-1, 0, 1] for j in [-1, 0, 1] if
+    (i != 0 or j != 0)
 ]
 
 
 def pieces_flipped(board, pos, current_player):
     """
-    Funkcja zwraca liste elementow do odwrocenia jesli gracz wykona wlasciwy ruch.
+    Funkcja zwraca liste elementow do
+    odwrocenia jesli gracz wykona wlasciwy ruch.
     :return : list of element to flip
     """
 
@@ -127,7 +153,6 @@ def pieces_flipped(board, pos, current_player):
 if __name__ == "__main__":
     from easyAI import Human_Player, AI_Player, Negamax
 
-    # An example: Computer vs Computer:
     game = Reversi([Human_Player(Negamax(4)), AI_Player(Negamax(4))])
     game.play()
     if game.scoring() > 0:
