972398039cc9ef2ec9ebcda1c304eb11eb1263c0
s13146-pj <56793702+s13146@users.noreply.github.com>
2022-12-02 09:19:14 +0100

    add task 4

--- 4/zadanie4_wine.py
+++ 4/zadanie4_wine.py
@@ -12,35 +12,44 @@
 # ---------------------------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import svm, datasets
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn import tree, svm, datasets
 
 
 def wine_calc():
-    """ Obcliczenie i wyświetlenie wykresu SVC dla wine dataset """
+    """ Obcliczenie i wyświetlenie wykresu dla wine dataset """
     wine = datasets.load_wine()
     print(wine)
-    X = wine["data"][:, (9, 1)]  # Color intensity,  Malic acid
-    y = wine["target"]
+    # Rozdzielamy dataset 75% traning / 25% test
+    X_train, X_test, y_train, y_test = train_test_split(
+        wine.data[:, :2], wine.target, test_size=0.25, random_state=100)
 
     # inicjalizacja SVC
-    svc = svm.SVC(kernel='rbf', C=1, gamma=10000).fit(X, y)
+    clf = svm.SVC(kernel='rbf', C=1, gamma=1000).fit(
+        X_train, y_train)
 
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    model_tree = tree.DecisionTreeClassifier()
+    model_tree.fit(X_train, y_train)
+
+    expected_y = y_test
+    predicted_y = model_tree.predict(X_test)
+
+    print(metrics.classification_report(expected_y, predicted_y))
+
+    x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
+    y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
     h = (x_max / x_min) / 100
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
-
     plt.subplot(1, 1, 1)
-    Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
-
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+    plt.scatter(wine.data[:, :2][:, 0], wine.data[:, :2][:, 1],
+                c=wine.target, cmap=plt.cm.Paired)
     plt.xlabel('Color Intensity')
     plt.ylabel('Malic Acid')
-    plt.xlim(xx.min(), xx.max())
-    plt.title('SVC with linear kernel')
     plt.show()
 
 
